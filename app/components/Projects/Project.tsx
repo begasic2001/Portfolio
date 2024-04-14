281095556ae52d3aa4bdcef2b6ed214d23aabe11
begasic2001 <luongminhthanh91@gmail.com>
2024-04-14 22:38:59 +0700

    styling project section p2

--- app/components/Projects/Project.tsx
+++ app/components/Projects/Project.tsx
@@ -1,31 +1,23 @@
 "use client";
 import ScreenHeading from "@/app/utils/ScreenHeading/ScreenHeading";
 import React from "react";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
 import dynamic from "next/dynamic";
-
 const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
   ssr: false,
 });
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Image from "next/image";
-
+import ProjectStyle from "../../styles/Project/Project.module.scss";
 const options = {
   loop: true,
-  margin: 10,
-  items: 1,
-  autoplay: true,
-
-  // loop: true,
-  // margin: 0,
+  margin: 0,
   nav: true,
   animateIn: "bounceInRight",
   animateOut: "bounceOutRight",
   dots: true,
-  // autoplay: true,
+  autoplay: true,
   smartSpeed: 1000,
   responsive: {
     0: {
@@ -44,20 +36,28 @@ const Project = () => {
   return (
     <div>
       <ScreenHeading title="Project" subHeading="Personal Project" />
-      <Container>
-        <Row>
-          <OwlCarousel className="owl-carousel owl-theme" id="" {...options}>
-            <Col lg={12}>
-              <div className="testi-item">
-                <div className="testi-comment">
-                  {/* <Image src={} width={100} height={100} /> */}
+      <div className={`${ProjectStyle.ProjectSection}`}>
+        <Container >
+          <Row>
+            <OwlCarousel
+              className="owl-carousel"
+              id="testimonial-carousel"
+              {...options}
+            >
+              <Col lg={12}>
+                <div className={`${ProjectStyle.ProjectItem}`}>
+                  <Image src="/bg-4.jpg" width={100} height={100} alt="" />
+
                   <p>Description project</p>
                 </div>
-              </div>
-            </Col>
-          </OwlCarousel>
-        </Row>
-      </Container>
+              </Col>
+            </OwlCarousel>
+          </Row>
+        </Container>
+      </div>
+      <div className={`${ProjectStyle.FooterImage}`}>
+        <img src="/wavefooterhero.png" alt="" />
+      </div>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
@@ -26,6 +25,14 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         ></link>
+        <script
+          src="https://code.jquery.com/jquery-3.7.1.slim.js"
+          integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc="
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+          integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
+        ></script>
       </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
