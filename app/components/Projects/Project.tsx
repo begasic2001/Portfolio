51ce3ffcfa133507e58bb8eeb26469f70c6bcf2e
begasic2001 <luongminhthanh91@gmail.com>
2024-04-23 00:13:51 +0700

    Set up to deploy 2

--- app/components/Projects/Project.tsx
+++ app/components/Projects/Project.tsx
@@ -4,9 +4,6 @@ import ScreenHeading from "../../utils/ScreenHeading/ScreenHeading";
 import React from "react";
 import dynamic from "next/dynamic";
 var $ = require("jquery");
-if (typeof window !== "undefined") {
-  window.$ = window.jQuery = require("jquery");
-}
 const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
   ssr: false,
 });
